7fd1ac7b425715619d1b12b89a1ce6e38f4e32bc
lucia-mn <lucmennah@alu.edu.gva.es>
2025-04-01 11:45:55 +0200

    Add files via upload

---
+++ lecturaEscrituraAjedrez/Ajedrez.java
@@ -0,0 +1,45 @@
+package lecturaEscrituraAjedrez;
+
+import java.io.*;
+import java.util.*;
+
+public class Ajedrez {
+    public static void main(String[] args) {
+
+        File j = new File("/home/lucmennah/IdeaProjects/lecturaEscritura/ajedrez/jugadoresAjedrez.txt");
+        File ja = new File("/home/lucmennah/IdeaProjects/lecturaEscritura/ajedrez/jugadoresAlojamiento.txt");
+
+        try (Scanner entrada = new Scanner(j);
+             PrintWriter salida = new PrintWriter(new FileWriter(ja))) {
+
+            List<String> jugadoresAlojamiento = new ArrayList<>();
+
+            while (entrada.hasNextLine()) {
+                String linea = entrada.nextLine();
+                String[] datos = linea.split(";");
+
+
+                if (datos.length >= 8 && datos[7].contains("H")) {
+                    String ranking = datos[0];
+                    String nombre = datos[2];
+                    jugadoresAlojamiento.add(ranking + ";" + nombre);
+                }
+            }
+
+
+            for (String jugador : jugadoresAlojamiento) {
+                salida.println(jugador);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("error: " + e.getMessage());
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("mal: " + e.getMessage());
+
+        } catch (IOException e) {
+            System.out.println("peor: " + e.getMessage());
+        }
+
+    }
+}
